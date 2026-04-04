e1dc5ffbe3bc8e025f5a578e12193dcda446133a
Ujjwal <tachyon1182@gmail.com>
2026-04-04 22:32:44 +0530

    Random comments for dropping DB info

--- start.sh
+++ start.sh
@@ -8,3 +8,16 @@ echo "Waiting for containers..."
 sleep 5
 docker ps
 uvicorn backend.main:app --reload
+
+
+# Temporrary reset commands for deleting -> Using currenlty to clean everything testing
+# Drop and recreate table (wipes all file records)
+#docker exec -it sfo_postgres psql -U sfo_user -d smart_file_organizer -c "DROP TABLE IF EXISTS files;"
+
+# Delete the entire collection
+# python3 -c "
+# import chromadb
+# client = chromadb.HttpClient(host='localhost', port=8001)
+# client.delete_collection('files')
+# print('ChromaDB collection deleted ✅')
+# "
